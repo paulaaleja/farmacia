4a0eba0180a56c8a95cc219ec7f4d0ce712dc030
paula garcia <103803748+paulaaleja@users.noreply.github.com>
2022-12-05 02:45:21 -0300

    Tab navigation

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import { useFonts } from "expo-font";
-import ShopNavigator from "./src/navigation/ShopNavigator";
+import BottomTabNavigator from "./src/navigation/BottomTabNavigator";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
-    Tillana: require("./src/assets/fonts/Roboto-Medium.ttf"),
+    Tillana: require("./src/assets/fonts/Tillana-SemiBold.ttf"),
   });
 
   if (!fontsLoaded) {
     return null;
   }
 
-  return <ShopNavigator />;
+  return <BottomTabNavigator />;
 }
